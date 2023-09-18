39c5c32a2b165c6a8bf268791c10ec969981735a
tray <onesimus.githua@student.moringaschool.com>
2023-09-18 21:27:16 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = 19; 
+
+if (random > 0 && Number.isInteger(random)) {
+  
+    console.log('is greater than 0.');
+} else {
+  
+    console.log('is not greater than 0 .');
+}
+const num3 = 12;
+const num4 = 8;
+const mod = (num3 % num4);
+console.log(mod);
+
+const numbers = [1,4,1,6,8,20];
+const max = Math.max(...numbers)
